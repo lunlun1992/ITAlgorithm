cc067d924b01bfe23cb698e8329efa8a100b66f0
Weilun Feng <lunlun1992@163.com>
2016-11-30 09:41:09 +0800

    finish chp14

--- chp14_augumenting_data_structures/rectangles_me.cpp
+++ chp14_augumenting_data_structures/rectangles_me.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 #define MAX 110
 struct Node
@@ -8,9 +8,9 @@ struct Node
 	double r;
 	double h;
 	bool flag;
-	bool operator()(Node *a, Node *b)
+	bool operator < (Node &a)
 	{
-		return a->h < b->h;
+		return h < a.h;
 	}
 }lines[MAX << 4];
 
@@ -18,13 +18,52 @@ bool cover[MAX << 4];
 double Xs[MAX << 4];
 double len[MAX << 4];
 
+void pushup(int l, int r, int treeidx)
+{
+	if (cover[treeidx])
+		len[treeidx] = Xs[r] - Xs[l];
+	else if (l + 1 == r)
+		len[treeidx] = 0;
+	else
+		len[treeidx] = len[treeidx << 1] + len[treeidx << 1 | 1];
+}
+
 void update(int l, int r, bool isin, int leftb, int rightb, int treeidx)
 {
-	
+	if(l == leftb && r == rightb)
+	{
+		cover[treeidx] = isin;
+		pushup(l, r, treeidx);
+		return;
+	}
+	int m = leftb + (rightb - leftb) / 2;
+	if (m >= r)
+		update(l, r, isin, leftb, m, treeidx << 1);
+	else if (m <= l)
+		update(l, r, isin, m, rightb, treeidx << 1 | 1);
+	else
+	{
+		update(l, m, isin, leftb, m, treeidx << 1);
+		update(m, r, isin, m, rightb, treeidx << 1 | 1);
+	}
+	pushup(leftb, rightb, treeidx);
 }
 
 int binarysearch(double d, int end)
 {
+	int s = 1;
+	int e = end;
+	while(s < e)
+	{
+		int m = s + ((e - s) >> 1);
+		if (Xs[m] == d)
+			return m;
+		else if (Xs[m] > d)
+			e = m - 1;
+		else
+			s = m + 1;
+	}
+	return s;
 }
 int main()
 {
@@ -36,7 +75,7 @@ int main()
 		int numx = 1;
 		for (int i = 0; i < n; i++)
 		{
-			scanf("%ld%ld%ld%ld", &x1, &y1, &x2, &y2);
+			scanf("%lf%lf%lf%lf", &x1, &y1, &x2, &y2);
 			Xs[numx] = x1;
 			lines[numx].l = x1;
 			lines[numx].r = x2;
@@ -59,6 +98,9 @@ int main()
 				Xs[k++] = Xs[i];
 		k--;
 		double ans = 0;
+		fill(cover, cover + (MAX << 4), false);
+		fill(len, len + (MAX << 4), 0);
+
 		for (int i = 1; i < numx; i++)
 		{
 			int l = binarysearch(lines[i].l, k);
@@ -66,8 +108,8 @@ int main()
 			update(l, r, lines[i].flag, 1, k, 1);
 			ans += len[1] * (lines[i + 1].h - lines[i].h);
 		}
-		printf("Test case #%d\n", t++);
-		printf("Total explored area: %.2lf\n\n", ans);
+		printf("Test case #%d\n", t++);
+		printf("Total explored area: %.2lf\n\n", ans);
 	}
 	return 0;
 }
